6d904b2b9dc30c43369b244a9ddb5cf97fad3cba
David T. <davidt.info@gmail.com>
2024-10-07 17:02:11 -0300

    Definindo ações para o filme

--- src/Filme.java
+++ src/Filme.java
@@ -2,7 +2,22 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacoes;
+    double somaDeAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDeAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDeAvaliacoes / totalDeAvaliacoes;
+    }
 }
