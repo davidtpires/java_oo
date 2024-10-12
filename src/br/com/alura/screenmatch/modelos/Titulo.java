a29439dee9413a11feceebecff57daaad30fb466
DavidT <davidt.info@gmail.com>
2024-10-12 14:07:46 -0300

    Transformando dados

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo>{
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDeAvaliacoes;
@@ -72,5 +76,9 @@ public class Titulo implements Comparable<Titulo>{
         return this.getNome().compareTo(outroTitulo.getNome());
     }
 
-
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento;
+    }
 }
