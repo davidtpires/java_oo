a6e4dabe40159e13ff41e2daf1f545cd32a2169a
DavidT <davidt.info@gmail.com>
2024-10-15 20:28:57 -0300

    Desafio Final Consolidando conhecimentos API+JSON)

--- src/br/com/alura/screenmatch/aulasAnteriores/desafiofinal/main/Main.java
+++ src/br/com/alura/screenmatch/aulasAnteriores/desafiofinal/main/Main.java
@@ -1,7 +1,30 @@
 package br.com.alura.screenmatch.aulasAnteriores.desafiofinal.main;
 
+import br.com.alura.screenmatch.aulasAnteriores.desafiofinal.model.ConsultaCep;
+import br.com.alura.screenmatch.aulasAnteriores.desafiofinal.model.Endereco;
+import br.com.alura.screenmatch.aulasAnteriores.desafiofinal.model.GeradorDeArquivo;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        System.out.println("Bem-vindo ao busca CEP!");
+        System.out.println("Digite um CEP para busca: (somente numeros)");
+        var cep = scanner.nextLine();
+
+        try{
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
 
     }
 }
